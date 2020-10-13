0c7fd38899a724f67d12680fe34062d198e91ae7
John Liu <abrn@users.noreply.github.com>
2020-10-13 10:09:28 +0100

    create constants.ts

---
+++ modules/constants.ts
@@ -0,0 +1,13 @@
+export const CHANNELS = {
+    bot_commands: "725042418251989064",
+    key_pops: "725694613893152828",
+    invalid_names: "747426430027169902",
+    null_names: "760121626183335936",
+    name_changes: "742468801311932468",
+    game_managers: "728599135242027131",
+    high_fame: "725728505714966579",
+    high_gold: "742818844933881888",
+    gold_purchases: "",
+    all_raids: "763388159857262592",
+    reaction_roles: "763727911177027586"
+}
